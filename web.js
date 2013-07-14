c8d0706cf42f2680c7a2138620a23ed5eaf04377
Muhammad Ibrahim <ibrahim@seas.harvard.edu>
2013-07-15 03:07:15 +0500

    added buffer

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var fs = require("fs")
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    html1 = fs.readFileSync("index.html")
-    response.send(html1);
+    var buffer = new Buffer()
+    index = fs.readFileSync("index.html")
+    buffer.write(index, "utf-8")
+    content = buffer.toString("utf-8")
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
