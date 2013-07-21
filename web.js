5275e79c3412cf24edbf57aecb1f3f0cede3de59
Muhammad Ibrahim <ibrahim@seas.harvard.edu>
2013-07-21 17:52:53 +0500

    trying buffer

--- web.js
+++ web.js
@@ -4,10 +4,8 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer(30)
     index = fs.readFileSync("index.html")
-    buffer.write(index, "utf-8")
-    content = buffer.toString("utf-8")
+    content = index.toString('utf-8')
     response.send(content);
 });
 
